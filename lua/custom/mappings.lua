1b1c92669734a93de98279803312e2e246549d73
Thamidu-Nadun <nadunrz101@gmail.com>
2025-10-20 01:45:10 +0530

    Update mapping.lua

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,9 @@
+-- Save in insert mode with Ctrl+S
+vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true })
+
+-- Save in normal mode with <leader>w (leader is usually space)
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+
+-- Comment when hit <C-/> in insert mode
+vim.api.nvim_set_keymap("i", "<C-c>", "<Esc><Plug>(comment_toggle_linewise_current)a", { noremap = false, silent = true })
+
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,13 @@
+local plugins = {
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-t>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+  }
+}
+
+return plugins
+
